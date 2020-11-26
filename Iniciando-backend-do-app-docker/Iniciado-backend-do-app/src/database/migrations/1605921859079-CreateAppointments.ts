fe3351c744f4d1d5a0e31e00cb6d85963afc1738
Matheus Santos <user@MacBook-Pro-de-User.local>
2020-11-26 16:08:38 -0300

    Banco de dados - Repositório do TypeORM(salvar progresso)

--- Iniciando-backend-do-app-docker/Iniciado-backend-do-app/src/database/migrations/1605921859079-CreateAppointments.ts
+++ Iniciando-backend-do-app-docker/Iniciado-backend-do-app/src/database/migrations/1605921859079-CreateAppointments.ts
@@ -1,6 +1,7 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateAppointments1605921859079 implements MigrationInterface {
+export default class CreateAppointments1605921859079
+  implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
