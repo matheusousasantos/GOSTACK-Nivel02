4702cbbaa6b0ddd7f37df3847770d66b18d819f0
Matheus Santos <user@MacBook-Pro-de-User.local>
2020-11-27 17:50:30 -0300

    Cadastro de Usuários - Criação de registros(0 - 11:48 min)

---
+++ Iniciando-backend-do-app-docker/Iniciado-backend-do-app/src/services/CreateUserService.ts
@@ -0,0 +1,13 @@
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    
+  }
+}
