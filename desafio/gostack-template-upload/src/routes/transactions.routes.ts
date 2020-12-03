b8cf5af43d5192596654f9cb8755ec8a0a63aa9e
Matheus Santos <matheus.ads.study@mail.com>
2020-12-02 22:59:30 -0300

    termino do desafio

--- desafio/gostack-template-upload/src/routes/transactions.routes.ts
+++ desafio/gostack-template-upload/src/routes/transactions.routes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
-
+import DeleteTransactionService from '../services/DeleteTransactionService';
 import TransactionsRepository from '../repositories/TransactionsRepository';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+import uploadConfig from '../config/upload';
+
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
+
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
   const transactions = await transactionsRepository.find();
@@ -30,11 +35,21 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const deleteTransaction = new DeleteTransactionService();
+  await deleteTransaction.execute(id);
+
+  return response.status(200).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransaction = new ImportTransactionsService();
+    const transactions = await importTransaction.execute(request.file.path);
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
