3b5334379e2622eabec0952bef70fd31d420e300
forthdev <forthdev@gmail.com>
2021-01-09 09:38:14 -0300

    Autenticação - Habilitando CORS na API(m: GSN3)

--- Iniciando-backend-do-app-docker/Iniciado-backend-do-app/src/server.ts
+++ Iniciando-backend-do-app-docker/Iniciado-backend-do-app/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import AppError from './errors/AppError';
@@ -8,6 +9,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
